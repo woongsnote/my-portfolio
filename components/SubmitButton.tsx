27078fa0756bcd9292dae11732a40c744d4bd716
woongsnote <woongsnote@gmail.com>
2023-10-12 14:58:59 +0900

    fix: fix import error

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@nextui-org/react";
 import { BsSend } from "react-icons/bs";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 const SubmitButton = () => {
   const { pending } = useFormStatus();
-  
+
   return (
     <Button
       color="primary"
@@ -12,8 +12,7 @@ const SubmitButton = () => {
       type="submit"
       className="mt-4 disabled:bg-gray-500"
       isLoading={pending}
-      isDisabled={pending}
-    >
+      isDisabled={pending}>
       {pending ? (
         <>전송중</>
       ) : (
